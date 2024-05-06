a0e0b7cfe65bfe2765546c9ad4399245e8a33488
BoH02 <jonathanaldo8@gmail.com>
2024-05-06 12:44:56 +0700

    Update NIM2213010362.java
    tambah gelar

--- NIM2213010362.java
+++ NIM2213010362.java
@@ -4,6 +4,6 @@
  */
 public class NIM2213010362 {
     public static void main(String[] args) {
-        System.out.println("Jonathan Aldo Setiawan");
+        System.out.println("Jonathan Aldo Setiawan, S.Kom");
     }
 }
